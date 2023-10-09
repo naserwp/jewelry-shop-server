13ecd6eb3658f91f4ff313cfa55e33493ec456ad
naserwp <naserwp@gmail.com>
2023-10-09 23:54:42 +0600

    done for server

--- index.js
+++ index.js
@@ -16,6 +16,26 @@ app.get('/jewelry', (req, res) => {
   res.send(jewelry);
 });
 
+// Create an API endpoint to fetch jewelry by ID
+app.get('/jewelry/:id', (req, res) => {
+    const { id } = req.params;
+    // Convert id to a number
+    const itemId = parseInt(id);
+  
+    const item = jewelry.find((item) => item.id === itemId);
+  
+    if (item) {
+      res.send(item);
+    } else {
+      res.status(404).send('Jewelry item not found');
+    }
+  });
+  
+
+
+
+
+
 app.listen(port, () => {
   console.log(`Jewelry Shop API is -Example app listening on port ${port}`);
 });
